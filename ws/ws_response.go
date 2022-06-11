afa654dcb479a9f4b65980be8bc0f9dce5ede9e6
chris_p9 <chris_powell9@outlook.com>
2022-06-11 23:24:27 +0100

    Fixing struct definitions to match perp futures API response

--- ws/ws_response.go
+++ ws/ws_response.go
@@ -25,14 +25,14 @@ func (o *OrderBookL2) Key() string {
 }
 
 type Trade struct {
-	Timestamp     time.Time `json:"timestamp"`
 	Symbol        string    `json:"symbol"`
-	Side          string    `json:"side"`
-	Size          int       `json:"size"`
-	Price         float64   `json:"price"`
 	TickDirection string    `json:"tick_direction"`
+	Price         string    `json:"price"`
+	Size          float64   `json:"size"`
+	Timestamp     time.Time `json:"timestamp"`
+	TradeTimeMs   string    `json:"trade_time_ms"`
+	Side          string    `json:"side"`
 	TradeID       string    `json:"trade_id"`
-	CrossSeq      int       `json:"cross_seq"`
 }
 
 type KLine struct {
